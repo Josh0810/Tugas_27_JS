dc4c266135532c4859c58ff85706858a30f604ab
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2022-01-05 21:42:41 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,14 @@
+function Tugas27() {
+    var a = "No";
+    var b = "rumah saya";
+    var c = 30;
+    var d = "A";
+    var no1 = a.concat(" ",b," ",c,d);
+    console.log(no1);
+
+    var e = "Jalan pangeran Tirtayasa No";
+    var f = 70;
+    var no2 = e.concat(" ",f);
+    console.log(no2);
+}
+Tugas27()
